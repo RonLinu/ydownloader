02d03cd09401e73a4d6109f44aef7c3b03b66b81
RonLinu <108692042+RonLinu@users.noreply.github.com>
2025-10-23 11:51:14 -0400

    Add files via upload
    First upload.

---
+++ resources/help.js
@@ -0,0 +1,11 @@
+window.HELP = `<html>
+Just paste a video URL (copied from your browser address bar) and click the 'Download' button.<br><br>
+
+These utilities must be installed on your system:<br>
+<li><i>yt-dlp</i> download utility<br>
+<li><i>ffmpeg</i> video conversion utility<br>
+<br>
+The Download Folder field is automatically filled with the default video folder
+of the detected OS but it can be overriden by selecting a different OS or
+by directly editing the field.<br>
+</html>`
