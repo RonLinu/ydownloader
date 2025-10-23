18e75ee1553dcd9ba141a1d0ebd431a459baf2d6
RonLinu <108692042+RonLinu@users.noreply.github.com>
2025-10-23 19:17:51 -0400

    Add files via upload
    New help text.

--- resources/help.js
+++ resources/help.js
@@ -1,16 +1,15 @@
 window.HELP = `
-These utilities must be installed on your system to be able to download videos:<br>
-<li><i>yt-dlp</i> download utility<br>
-<li><i>ffmpeg</i> video conversion utility<br>
-<br>
-To download a video, just paste its video URL (copied from a browser address bar) and click the 'Download' button.
+To download a video, just paste its URL (copied from a browser address bar) and click the 'Download' button.
+<br><br>
+Beware that these utilities must be installed on your system to be able to download videos:<br>
+<li><i>yt-dlp</i> download utility</li>
+<li><i>ffmpeg</i> video conversion utility</li>
 `
 
 window.BRIDGE = `
-This web page must be opened by the program <i>ydownloader</i>
-that comes with this application. Otherwise the Download button is disabled.<br><br>
-<i>ydownloader</i> acts as a bridge between this web page and the operating
-system that can execute the <i>yt-dlp</i> utility.
-Once the bridge is up and running, this web page is opened automatically in your default browser.<br><br>
-This message will also appears if you try to reload this web page manually.
+This is a web application and it must be launched with the <i>ydownloader</i> program.
+A link or a bookmark to this page do not work.<br><br>
+<i>ydownloader</i> also acts as a bridge between the web browser and the operating
+system. It is needed to execute external programs like the <i>yt-dlp</i> utility.<br><br>
+This message will also appear if you try to reload this web page manually.
 `
