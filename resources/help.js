46f5022b61c55c883e554d510d3a88b765c14697
RonLinu <108692042+RonLinu@users.noreply.github.com>
2025-10-24 07:39:17 -0400

    Add files via upload

--- resources/help.js
+++ resources/help.js
@@ -7,9 +7,9 @@ Beware that these utilities must be installed on your system to be able to downl
 `
 
 window.BRIDGE = `
-This is a web application and it must be launched with the <i>ydownloader</i> program.
-A link or a bookmark to this page do not work.<br><br>
-<i>ydownloader</i> also acts as a bridge between the web browser and the operating
-system. It is needed to execute external programs like the <i>yt-dlp</i> utility.<br><br>
+This is a web application and it must be launched with the <i>ydownloader</i> executable program,
+not directly from a web browser with a link or a bookmark.<br><br>
+Your web browser is conveniently used as a front end to the application.
+<br><br>
 This message will also appear if you try to reload this web page manually.
 `
