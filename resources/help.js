03c251f97d2b00e49ef075213a72f5aaed11be38
RonLinu <108692042+RonLinu@users.noreply.github.com>
2025-10-25 16:11:50 -0400

    Add files via upload
    Adding  "xterm' as a requirement

--- resources/help.js
+++ resources/help.js
@@ -2,14 +2,15 @@ window.HELP = `
 To download a video, just paste its URL (copied from a browser address bar) and click the 'Download' button.
 <br><br>
 Beware that these utilities must be installed on your system to be able to download videos:<br>
-<li><i>yt-dlp</i> download utility</li>
-<li><i>ffmpeg</i> video conversion utility</li>
+- <i>yt-dlp</i> download utility<br>
+- <i>ffmpeg</i> video conversion utility<br>
+- <i>xterm</i> terminal (for Linux/MacOS only)
 `
 
 window.BRIDGE = `
 This is a web application and it must be launched with the <i>ydownloader</i> executable program,
 not directly from a web browser with a link or a bookmark.<br><br>
-Your web browser is conveniently used as a front end to the application.
+Your default web browser is used as a front end to the application.
 <br><br>
 This message will also appear if you try to reload this web page manually.
 `
