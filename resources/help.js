9e02e4a83dbbeab8f702176a7226950b2f837eca
RonLinu <108692042+RonLinu@users.noreply.github.com>
2025-10-27 16:55:28 -0400

    Add files via upload
    Cleanup help text.

--- resources/help.js
+++ resources/help.js
@@ -1,16 +1,11 @@
 window.HELP = `
 To download a video, just paste its URL (copied from a browser address bar) and click the 'Download' button.
 <br><br>
-Beware that these utilities must be installed on your system to be able to download videos:<br>
-- <i>yt-dlp</i> download utility<br>
-- <i>ffmpeg</i> video conversion utility<br>
-- <i>xterm</i> terminal (for Linux/MacOS only)
-`
-
-window.BRIDGE = `
-This is a web application and it must be launched with the <i>ydownloader</i> executable program,
-not directly from a web browser with a link or a bookmark.<br><br>
-Your default web browser is used as a front end to the application.
-<br><br>
-This message will also appear if you try to reload this web page manually.
+The following utilities must be installed on your system and
+in your PATH to be able to download videos:
+<pre>
+- <i>yt-dlp</i> download utility
+- <i>ffmpeg</i> video conversion utility
+- <i>xterm</i>  terminal utility (Linux/MacOS only)
+</pre>
 `
