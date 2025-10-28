705d8ea63ffbb9164c9b0843875aeda6631a9aee
RonLinu <108692042+RonLinu@users.noreply.github.com>
2025-10-28 07:40:58 -0400

    Update ydownloader.sh to use a remote URL

--- websocket/ydownloader.sh
+++ websocket/ydownloader.sh
@@ -2,6 +2,4 @@ cd "$(dirname "$0")"
 
 npx kill-port 8080
 
-node websocket.js file:///home/ronald/Documents/CoffeeScript/ydownloader/index.html 8080
-
-#~ node websocket.js https://ronlinu.github.io/ydownloader 8080
+node websocket.js https://ronlinu.github.io/ydownloader 8080
