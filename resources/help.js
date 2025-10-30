f7427af5b8aa6541dc21ba3aad975b85019d4442
RonLinu <108692042+RonLinu@users.noreply.github.com>
2025-10-30 10:12:38 -0400

    Add files via upload
    Help text updated.

--- resources/help.js
+++ resources/help.js
@@ -6,6 +6,6 @@ in your PATH to be able to download videos:
 <pre>
 - <b>yt-dlp</b> video download utility
 - <b>ffmpeg</b> video conversion utility
-- <b>xterm</b>  terminal utility (Linux/MacOS only)
+- <b>xterm</b>  terminal utility (Linux only)
 </pre>
 `
