abda99fe02f3ba160bcf47d55d06084d42359482
RonLinu <108692042+RonLinu@users.noreply.github.com>
2025-11-03 09:10:14 -0500

    Add files via upload

--- resources/help.js
+++ resources/help.js
@@ -1,11 +1,11 @@
 window.HELP = `
 To download a video, just paste its URL (copied from a browser address bar) and click the 'Download' button.
 <br><br>
-The following utilities must be installed on your system and
+The following dependencies must be installed on your system and
 in your PATH to be able to download videos:
 <pre>
-- <b>yt-dlp</b> video download utility
-- <b>ffmpeg</b> video conversion utility
-- <b>xterm</b>  terminal utility (Linux only)
+- <b>yt-dlp</b> video download utility</b>
+- <b>ffmpeg</b> video conversion utility</b>
 </pre>
+To verify, click the 'Check dependencies' button.
 `
