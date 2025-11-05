9269518a5dd9ebf7416f6d79c0733e976b1dc8b6
RonLinu <108692042+RonLinu@users.noreply.github.com>
2025-11-05 08:15:38 -0500

    Add files via upload

--- resources/help.js
+++ resources/help.js
@@ -1,5 +1,5 @@
 window.HELP = `
-To download a video, just paste its URL (copied from a browser address bar) and click the 'Download' button.
+To download a video, just paste its URL (ex: copied from a browser address bar) and click the 'Download' button.
 <br><br>
 The following dependencies must be installed on your system and
 in your PATH to be able to download videos:
@@ -7,5 +7,5 @@ in your PATH to be able to download videos:
 - <b>yt-dlp</b> video download utility</b>
 - <b>ffmpeg</b> video conversion utility</b>
 </pre>
-To verify this, click the 'Check dependencies' button.
+To verify this, click 'Check dependencies' button.
 `
