4e1add3c267f08b33d45ab00c88813d380f5c85b
RonLinu <108692042+RonLinu@users.noreply.github.com>
2025-11-13 08:02:50 -0500

    Add files via upload
    Help text change.

--- resources/help.js
+++ resources/help.js
@@ -2,8 +2,12 @@ window.help = `
 To download a video, paste its URL in the 'Video URL' field and click the 'Download' button.
 A terminal window will open showing the download progress.
 <br><br>
-In case of download error, try 'Resolution cap' = <b>No cap</b>.
-<br><br>
-Note that <i>yt-dlp</i> may not be able to download some videos due
-to proprietary video formats or web site restrictions.
+In case of download error, try 'Resolution cap' = <b>No cap</b>.<br>
+<br>
+<hr>
+<br>
+Because of the ever-changing video environment on social media, 
+YDownloader is only as good as <i>yt-dlp</i> can be.<br><br>
+Downloading may be incomplete or not possible because of proprietary video
+formats, web site restrictions, or new video URL structures.
 `
